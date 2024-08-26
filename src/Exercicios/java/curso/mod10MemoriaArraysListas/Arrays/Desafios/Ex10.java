ce63e744b2c5fe7af58f158601862df1661cc6e4
França <mars.cesc@gmail.com>
2024-08-26 20:12:03 -0300

    Exercicios de fixação de manipulação de arrays.

---
+++ src/Exercicios/java/curso/mod10MemoriaArraysListas/Arrays/Desafios/Ex10.java
@@ -0,0 +1,57 @@
+package Exercicios.java.curso.mod10MemoriaArraysListas.Arrays.Desafios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+
+
+        System.out.println("Qual quantidade de alunos matriculados?: ");
+        int quantity = read.nextInt();
+        read.nextLine();
+
+        String[] names = new String[quantity];
+        double[] notes1 = new double[quantity];
+        double[] notes2 = new double[quantity];
+
+        int orderPeople = 1;
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Digite os dados do " + orderPeople + "ª aluno(a): ");
+
+            System.out.println("Nome: ");
+            names[i] = read.nextLine();
+
+            System.out.println("Nota 1: ");
+            notes1[i] = read.nextDouble();
+
+            System.out.println("Nota 2: ");
+            notes2[i] = read.nextDouble();
+
+            read.nextLine();
+            orderPeople++;
+        }
+
+        double halfYear1 = notes1[0];
+        double halfYear2 = notes2[0];
+        double approved = 0;
+        String alunoApproved = "";
+        for (int i = 0; i < quantity; i++) {
+            if (halfYear1 + halfYear2 / 2 >= 6) {
+                approved = notes1[i] + notes2[i];
+                alunoApproved = names[i];
+            }
+        }
+
+        if (approved / 2 >= 6) {
+            System.out.println("Alunos aprovados: ");
+            System.out.println(alunoApproved);
+            System.out.println(approved / quantity);
+        } else System.out.println("Não há alunos aprovados!");
+
+        read.close();
+    }
+}
---
+++ src/Exercicios/java/curso/mod10MemoriaArraysListas/Arrays/Desafios/Ex11.java
@@ -0,0 +1,73 @@
+package Exercicios.java.curso.mod10MemoriaArraysListas.Arrays.Desafios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Quantos quer armazenar?: ");
+        int quantity = read.nextInt();
+        read.nextLine();
+
+        String[] genders = new String[quantity];
+        double[] heights = new double[quantity];
+
+        int order = 1;
+        for (int i = 0; i < quantity; i++) {
+            System.out.println("Digite os dados da "+ order +"ª pessoa: ");
+
+            System.out.print("altura: ");
+            heights[i] = read.nextDouble();
+
+            read.nextLine();
+
+            System.out.print("Genero: ");
+            genders[i] = read.nextLine();
+
+            order++;
+        }
+
+        double lowerHeight = heights[0];
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] <= lowerHeight) {
+                lowerHeight = heights[i];
+            }
+        }
+
+        double greaterHeight = heights[0];
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] >= greaterHeight) {
+                greaterHeight = heights[i];
+            }
+        }
+
+        int women = 0;
+        double heightWomen = 0;
+        for (int i = 0; i < genders.length; i++) {
+            if (genders[i].contains("F") || genders[i].contains("f")) {
+                heightWomen += heights[i];
+                women++;
+            }
+        }
+        
+        double mediaHeightWomen = heightWomen / women;
+
+        int men = 0;
+        for (int i = 0; i < genders.length; i++) {
+            if (genders[i].contains("M") || genders[i].contains("m")) {
+                men++;
+            }
+        }
+
+        System.out.println();
+        System.out.printf("Menor altura: %.2f\n", lowerHeight);
+        System.out.printf("Maior altura: %.2f\n", greaterHeight);
+        System.out.printf("Média das alturas das mulheres: %.2f\n", mediaHeightWomen);
+        System.out.println("Quantidade de homens na pesquisa: "+ men);
+
+        read.close();
+    }
+}
