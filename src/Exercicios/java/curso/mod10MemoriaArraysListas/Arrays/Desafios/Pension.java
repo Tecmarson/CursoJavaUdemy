b266952f80115b2f1308063a51f7c39ac4016b66
França <mars.cesc@gmail.com>
2024-09-02 19:04:08 -0300

    Exercicios de fixação de manipulação de arrays. Apenas reduzi um loop colocando as operações que nele haviam em outro for que ja possuia rotinas, reduzindo as linhas de código.

--- src/Exercicios/java/curso/mod10MemoriaArraysListas/Arrays/Desafios/Pension.java
+++ src/Exercicios/java/curso/mod10MemoriaArraysListas/Arrays/Desafios/Pension.java
@@ -3,19 +3,19 @@ package Exercicios.java.curso.mod10MemoriaArraysListas.Arrays.Desafios;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Pensionato {
+public class Pension {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner read = new Scanner(System.in);
-        Rent[] rooms = new Rent[10];
+        Rent[] roomsForRent = new Rent[10];
 
         System.out.println("How many rooms will be rented? ");
-        int roomsQuantity = read.nextInt();
+        Integer roomsQuantity = read.nextInt();
         read.nextLine();
 
         for (int i = 0; i < roomsQuantity; i++) {
             System.out.println();
-            System.out.print("Rent #" + (i+1) + "\n");
+            System.out.println("Rent #" + (i+1));
             System.out.println("What´s your name?");
             System.out.print("name: ");
             String name = read.nextLine();
@@ -26,9 +26,9 @@ public class Pensionato {
 
             System.out.println("Which room do you want?");
             System.out.print("room: ");
-            int roomNumber = read.nextInt();
+            Integer roomNumber = read.nextInt();
 
-            rooms[roomNumber] = new Rent(name, email);
+            roomsForRent[roomNumber] = new Rent(name, email);
 
             read.nextLine();
 
@@ -36,8 +36,8 @@ public class Pensionato {
 
 
         System.out.println("Busy rooms: ");
-        for (int i = 0; i < rooms.length; i++) {
-            if (rooms[i] != null) System.out.println(i + ": " + rooms[i]);
+        for (int i = 0; i < roomsForRent.length; i++) {
+            if (roomsForRent[i] != null) System.out.println(i + ": " + roomsForRent[i]);
         }
 
         read.close();
